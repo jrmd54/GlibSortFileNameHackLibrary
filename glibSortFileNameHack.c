72733a084c26f50d8c3ae0162f1241e73bb74388
jdesroziers <j.desroziers@cyber-detect.eu>
2025-06-06 12:10:07 +0200

    only use raw ordering for md5s and shas

--- glibSortFileNameHack.c
+++ glibSortFileNameHack.c
@@ -5,13 +5,34 @@
 
 gchar *
 (*real_g_utf8_collate_key_for_filename)(const gchar *str,
-				 gssize       len);
+                 gssize       len);
 
 gchar *
 g_utf8_collate_key_for_filename(const gchar *str,
-				 gssize       len)
+                 gssize       len)
 {
-  return g_utf8_collate_key(str, len);
+  // gssize: len of str OR -1 if null-terminated
+  if (len >= 0) {
+      len = (gsize)len;
+  } else {
+      len = strlen(str);
+  }
+
+  // raw byte ordering for hashs (05e.. < 517.. < 2f5..)
+  if (len == 32 || len == 64) { // md5 or sha
+    return g_utf8_collate_key(str, len);
+  }
+
+  // numerical ordering (2f5.. < 05e.. < 517..)
+  if (!real_g_utf8_collate_key_for_filename) {
+    real_g_utf8_collate_key_for_filename = dlsym(RTLD_NEXT, "g_utf8_collate_key_for_filename");
+    if (!real_g_utf8_collate_key_for_filename) {
+      fprintf(stderr, "Error loading original function: %s\n", dlerror());
+      return NULL;
+    }
+  }
+
+  return real_g_utf8_collate_key_for_filename(str, len);
 }
 
 
@@ -20,3 +41,4 @@ void _init(void)
 // printf("Loading hack\n");
  real_g_utf8_collate_key_for_filename = dlsym(RTLD_NEXT, "g_utf8_collate_key_for_filename");
 }
+
